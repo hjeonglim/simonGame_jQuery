34eb2692fb6c0439b8aa8f42ce920b7ecdd38253
Hannah Lim <hjeong.lim@gmail.com>
2020-09-21 22:08:10 -0700

    Add level updating feature

--- game.js
+++ game.js
@@ -3,6 +3,20 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started = false;
+
+// Create a new variable called level and start at level 0
+
+var level = 0;
+
+$(document).keypress(function() {
+    if (!started) {
+        $("#level-title").text("Level" + level);
+        nextSequence();
+        started = true;
+    }
+});
 
 // Detect click
 
@@ -12,43 +26,28 @@ $(".btn").click(function() {
     userClickedPattern.push(userChosenColour);
 
     playSound(userChosenColour);
-
     animatePress(userChosenColour);
 });
-
-
-// Start the game
-
-document.addEventListener("keypress", nextSequence());
-
-while (var level=0) {
-
-    if (level === 0) {
-        $("#level-title").innerHTML = `LEVEL ${level}`;
-        nextSequence();
-    }
-    level++;
-}
 
 // create function to get the random color box
 
 function nextSequence(){
+
+    // Increase the level by 1 every time nextSequence() is called
+    level++;
+
+    // Update the h1 with this change in the value of Level
+    $("#level-title").text("Level " + level);
 
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
     playSound(randomChosenColour);
 }
 
-
-
-
-
 // play the sounds
-
 function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
@@ -57,9 +56,7 @@ function playSound(name) {
 // animate a flash to the button selected in randomChosenClour
 
 function animatePress(currentColor) {
-
     $("#" + currentColor).addClass("pressed");
-
     setTimeout(function() {
         $("#" + currentColor).removeClass("pressed");
     }, 100);
